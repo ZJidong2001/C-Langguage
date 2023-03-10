fcb049ef91dc8a1067313927a180c3a7fe299a74
张济东 <1436581890@qq.com>
2023-03-10 14:28:29 +0800

    2021_06_07

---
+++ 2021_06_07/add.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//全局变量
+//static 修饰全局变量
+//static int g_val = 2021;
+//
+//
+////函数是具有外部链接属性的
+//static int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+
+
